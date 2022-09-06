190de19fbb59cf850ddbd768388b458e1483aa85
walerikq <walerikq@yandex.ru>
2022-09-06 22:53:39 +0500

    update

--- src/finances/Account.java
+++ src/finances/Account.java
@@ -1,10 +1,46 @@
 package finances;
 
 public class Account {
-    private int income;
-    private int expense;
     private int score;
+    private int expence;
+    private int income;
 
+    public Account(int score,int expence, int income){
+        this.expence = expence;
+        this.score = score;
+        this.income = income;
+    }
 
+    public int getExpence() {
+        return expence;
+    }
 
+    public int getIncome() {
+        return income;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setExpence(int expence) {
+        this.expence = expence;
+    }
+
+    public void setIncome(int income) {
+        this.income = income;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    @Override
+    public String toString() {
+        return "Счёт" +
+                "score=" + score +
+                ", expence=" + expence +
+                ", income=" + income +
+                '}';
+    }
 }
--- src/finances/Finances.java
+++ src/finances/Finances.java
@@ -1,7 +1,66 @@
 package finances;
 
-public class Finances {
-    public static void main(String[] args) {
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
+public class Finances {
+//    final static File file = new File("accounts.txt");
+
+    public static void main(String[] args) {
+        ArrayList<Account> accounts = new ArrayList<>();
+        Account account1 = new Account(0,0,0);
+        Account contributionAccount = new Account(0,0,0);
+        accounts.add(0,account1);
+        accounts.add(1,contributionAccount);
+        menu(accounts);
     }
+
+    public static void menu(List<Account> list) {
+        Scanner in = new Scanner(System.in);
+        while (true) {
+            System.out.println("Выберите нужный пункт меню:");
+            System.out.println("1 - Баланс");
+            System.out.println("2 - Пополнить баланс");
+            System.out.println("3 - Указать расходы");
+            System.out.println("5 - Выписка по счёту");
+            System.out.println("6 - Создать новый счёт");
+            System.out.println("7 - Выбрать счёт");
+
+            System.out.println("0 - Выход.");
+
+            int num = in.nextInt();
+            switch (num) {
+                case 0:
+                    System.exit(0);
+                case 1:
+
+                case 2:
+
+                case 3:
+
+                case 4:
+
+                case 5:
+
+                case 6:
+
+                case 7:
+                    printFigures(list);
+            }
+        }
+    }
+    /**
+     * метод для вывода фигур из списка
+     */
+    static void printFigures(List<Account> accounts) {
+        int i = 1;
+        for (Account f : accounts) {
+            System.out.println(i + ". " + f);
+            i++;
+        }
+    }
+
+
 }
